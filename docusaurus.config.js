3ad08f9041182c6b4bf8132044867f848b71cdad
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2021-11-01 10:58:15 -0400

    Add PWA support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,58 +3,114 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'IPC144 - Course Notes',
-  tagline: 'C Programming',
-  url: 'https://cghub.ca',
-  baseUrl: '/',
-  trailingSlash: false,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Seneca-ICTOER',
-  projectName: 'IPC144',
-  themeConfig: {
-    navbar: {
-      title: 'IPC144',
-      logo: {
-        alt: 'Seneca College',
-        src: 'img/logo.svg',
-        srcDark: 'img/logo-dark.svg',
-      },
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'IPC144 - Course Notes',
-          items: [
-            {
-              label: 'Contents',
-              to: '/',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Seneca College.`,
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-    		  routeBasePath: '/',
-          editUrl: 'https://github.com/Seneca-ICTOER/IPC144/tree/main',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
-  ],
+	title: 'IPC144 - Course Notes',
+	tagline: 'C Programming',
+	url: 'https://cghub.ca',
+	baseUrl: '/',
+	trailingSlash: false,
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'Seneca-ICTOER',
+	projectName: 'IPC144',
+	themeConfig: {
+		navbar: {
+			title: 'IPC144',
+			logo: {
+				alt: 'Seneca College',
+				src: 'img/logo.svg',
+				srcDark: 'img/logo-dark.svg',
+			},
+		},
+		footer: {
+			style: 'dark',
+			links: [
+				{
+					title: 'IPC144 - Course Notes',
+					items: [
+						{
+							label: 'Contents',
+							to: '/',
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} Seneca College.`,
+		},
+		prism: {
+			theme: lightCodeTheme,
+			darkTheme: darkCodeTheme,
+		},
+	},
+	presets: [
+		[
+			'@docusaurus/preset-classic',
+			{
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					routeBasePath: '/',
+					editUrl: 'https://github.com/Seneca-ICTOER/IPC144/tree/main',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			},
+		],
+	],
+	plugins: [
+		[
+			'@docusaurus/plugin-pwa',
+			{
+				offlineModeActivationStrategies: ['standalone', 'queryString'],
+				pwaHead: [
+					{
+						tagName: 'link',
+						rel: 'icon',
+						href: 'img/pwa/manifest-icon-512.png',
+					},
+					{
+						tagName: 'link',
+						rel: 'manifest',
+						href: '/manifest.json',
+					},
+					{
+						tagName: 'meta',
+						name: 'theme-color',
+						content: '#DA291C',
+					},
+					{
+						tagName: 'meta',
+						name: 'apple-mobile-web-app-capable',
+						content: 'yes',
+					},
+					{
+						tagName: 'meta',
+						name: 'apple-mobile-web-app-status-bar-style',
+						content: '#DA291C',
+					},
+					{
+						tagName: 'link',
+						rel: 'apple-touch-icon',
+						href: 'img/pwa/manifest-icon-192.png',
+					},
+					{
+						tagName: 'link',
+						rel: 'mask-icon',
+						href: 'img/pwa/manifest-icon-512.png',
+						color: '#DA291C',
+					},
+					{
+						tagName: 'meta',
+						name: 'msapplication-TileImage',
+						content: 'img/pwa/manifest-icon-512.png',
+					},
+					{
+						tagName: 'meta',
+						name: 'msapplication-TileColor',
+						content: '#DA291C',
+					},
+				],
+			},
+		],
+	],
 };
