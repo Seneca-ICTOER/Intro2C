bdffe4cf9d999268794d58c82e4c6586e343875e
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2021-11-01 11:16:38 -0400

    Fix yarn.lock conflict

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,60 +3,60 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-	title: 'IPC144 - Course Notes',
-	tagline: 'C Programming',
-	url: 'https://cghub.ca',
-	baseUrl: '/',
-	trailingSlash: false,
-	onBrokenLinks: 'throw',
-	onBrokenMarkdownLinks: 'warn',
-	favicon: 'img/favicon.ico',
-	organizationName: 'Seneca-ICTOER',
-	projectName: 'IPC144',
-	themeConfig: {
-		navbar: {
-			title: 'IPC144',
-			logo: {
-				alt: 'Seneca College',
-				src: 'img/logo.svg',
-				srcDark: 'img/logo-dark.svg',
-			},
-		},
-		footer: {
-			style: 'dark',
-			links: [
-				{
-					title: 'IPC144 - Course Notes',
-					items: [
-						{
-							label: 'Contents',
-							to: '/',
-						},
-					],
-				},
-			],
-			copyright: `Copyright © ${new Date().getFullYear()} Seneca College.`,
-		},
-		prism: {
-			theme: lightCodeTheme,
-			darkTheme: darkCodeTheme,
-		},
-	},
-	presets: [
-		[
-			'@docusaurus/preset-classic',
-			{
-				docs: {
-					sidebarPath: require.resolve('./sidebars.js'),
-					routeBasePath: '/',
-					editUrl: 'https://github.com/Seneca-ICTOER/IPC144/tree/main',
-				},
-				theme: {
-					customCss: require.resolve('./src/css/custom.css'),
-				},
-			},
-		],
-	],
+  title: 'IPC144 - Course Notes',
+  tagline: 'C Programming',
+  url: 'https://cghub.ca',
+  baseUrl: '/',
+  trailingSlash: false,
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Seneca-ICTOER',
+  projectName: 'IPC144',
+  themeConfig: {
+    navbar: {
+      title: 'IPC144',
+      logo: {
+        alt: 'Seneca College',
+        src: 'img/logo.svg',
+        srcDark: 'img/logo-dark.svg',
+      },
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'IPC144 - Course Notes',
+          items: [
+            {
+              label: 'Contents',
+              to: '/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Seneca College.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+    		  routeBasePath: '/',
+          editUrl: 'https://github.com/Seneca-ICTOER/IPC144/tree/main',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
 	plugins: [
 		[
 			'@docusaurus/plugin-pwa',
