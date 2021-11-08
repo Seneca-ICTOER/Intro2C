00cb6ed789149674fb8dc47a0f70cadb3170c6d1
Kien <kiennguyenchi11@gmail.com>
2021-11-08 14:43:02 -0500

    add Weekly Schedule to footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,10 @@ module.exports = {
               label: 'Contents',
               to: '/',
             },
+			{
+			  label: 'Weekly Schedule',
+			  to: '/weeklyContents',
+			},
           ],
         },
       ],
