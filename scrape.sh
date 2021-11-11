fc91f685ee4b15b0be27f60c68998c923ac4f670
ycechungAI <ycechung@myseneca.ca>
2021-11-11 11:45:03 -0500

    *removed url-loader *added comments to code *fixed some parts

--- scrape.sh
+++ scrape.sh
@@ -1,7 +1,21 @@
+#if the .env file exists, then export the environment variables from it
+#.env file sample:
+#=============================================
+#APPLICATION_ID=6TZGXYS27N
+#API_KEY=51151f6285b830cea1d7e40675f8af8f
+#SITE_URL=https://cghub.ca
+#INDEX_NAME=docusaurus
+#=============================================
 if [ -f .env ]; then
     export $(xargs < .env)
 fi
 
+# run the docker container with the following arguments:
+#    -it: interactive mode
+#    --env-file=.env: the environment variables are set from the .env file
+#    -e "CONFIG=$(cat ./config.json | jq -r tostring)": the config.json file is passed as an environment variable need to install jq python library if you dont have it
+# the container will run the bash script that will run the scraper
+# The scraper will run in the background, and the container will exit.
 sudo docker run \
         -it --env-file=.env -e "CONFIG=$(cat ./config.json | jq -r tostring)" \
         algolia/docsearch-scraper
