ff0a9a5d6061a21813490e3329c5934bdc8eaeb1
ycechungAI <ycechung@myseneca.ca>
2021-11-11 06:18:11 -0500

    semi-working code on linux

--- scrape.sh
+++ scrape.sh
@@ -2,8 +2,7 @@ if [ -f .env ]; then
     export $(xargs < .env)
 fi
 
-docker run \
-	-e APPLICATION_ID=$(printenv APPLICATION_ID) \
-	-e API_KEY=$(printenv API_KEY) \
-	-e CONFIG="$(cat config.json | jq '.start_urls=[`https://cghub.ca`] | jq '.index_name=docusaurus' | jq -r tostring)" \
-	algolia/docsearch-scraper:v1.13.0
+sudo docker run \
+        -it --env-file=.env -e "CONFIG=$(cat ./config.json | jq -r tostring)" \
+        algolia/docsearch-scraper
+
