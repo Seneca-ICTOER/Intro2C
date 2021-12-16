dc85fe64d825eff67a6ce770aa32a75d2eb9a207
David Humphrey <david.andrew.humphrey@gmail.com>
2021-12-16 11:30:09 -0500

    Fix execa syntax for yarn commands

--- convert-to-pdf.js
+++ convert-to-pdf.js
@@ -5,18 +5,18 @@ const opts = {
   resources: [`http://localhost:${PORT}/`],
 };
 const serve = async () => {
-  const { stdout, stderr } = await execa('yarn serve');
+  const { stdout, stderr } = await execa('yarn', ['serve']);
   console.log(stdout);
   console.error(stderr);
 };
 const killPort = async () => {
-  const { stdout, stderr } = await execa(`npx kill-port ${PORT}`);
+  const { stdout, stderr } = await execa(`npx`, ['kill-port', PORT]);
   console.log(stdout);
   console.error(stderr);
   process.exit(0);
 };
 const toPdf = async () => {
-  const { stdout, stderr } = await execa('yarn to-pdf');
+  const { stdout, stderr } = await execa('yarn', ['to-pdf']);
   console.log(stdout);
   console.error(stderr);
   killPort();
